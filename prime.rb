2ec49dfc240dbbc399d31d608f50a26024403484
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 23:12:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,18 @@
 # Add  code here!
 def prime?(number)
       if number >= 2
-            i = 2
-            while i < number-1 do
-                  return false if number % i == 0
-                  i += 1
-            end
+            array = (2..number-1).to_a
+            array.all? { |e|  number % e == 0}? false : true
+
+
+
+
+            # i = 2
+            # array = []
+            # while i < number-1 do
+            #       array << false if number % i == 0
+            #       i += 1
+            # end
       else
             false
       end

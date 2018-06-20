3a7cec4ea6eef0990cd9f1ae073d227d791854ea
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 22:57:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(number)
       if number >= 2
             for i in 2..number-1 do
-                  number % i != 0 ? false : true
+                  number % i == 0 ? true : false
             end
       else
             false
